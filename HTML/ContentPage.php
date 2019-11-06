b73519ba2d4e1310a367faa79dd17cb3c670d6ef
Andre Silva <38663710+Drexxxxxxxxx@users.noreply.github.com>
2019-11-06 12:46:14 +0000

    Styles and Colors for website

--- HTML/ContentPage.php
+++ HTML/ContentPage.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php
     include_once "Content/_header.html";
-    ?>    
-    <link rel="stylesheet" href="../PHP/Style.php/Homepage.scss">
+    ?>
+    <link rel="stylesheet" href="../PHP/Style.php/Generic.scss">
     <script src="../JS/HomePage.js"></script>
-    <title>Homepage</title>
+    <title>Content Page</title>
 </head>
-<?php
-include_once "../PHP/HomePageHelper.php";
-?>
+
 <body onload="GetWorkingExample();">
     <?php
     include_once "Content/Navbar.html";
     ?>
-    <div id="Receiver"></div>
-    <h1>TESTE</h1>
+
+    <div class="container">
+        <h1>TESTE</h1>
+        <h2>TESTE</h2>
+        <h3>TESTE</h3>
+        <h4>TESTE</h4>
+        <h5>TESTE</h5>
+        <h6>TESTE</h6>
+        <p>Lorem Ipsum</p>
+    </div>
+
+
+
+
 </body>
 <?php
-    include_once "Content/_scripts.html";
-    ?>    
+include_once "Content/_scripts.html";
+?>
+
 </html>
