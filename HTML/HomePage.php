716499f6ca65eb00acb12322052d4a05241b7a5d
miguel9177 <miguel.128.b80@gmail.com>
2019-11-26 23:22:40 +0000

    css homepage footer

--- HTML/HomePage.php
+++ HTML/HomePage.php
@@ -24,26 +24,52 @@ include_once "../PHP/HomePageHelper.php";
     <div class="container-fluid">
         <div class="row">
             <div class="Slidshow col-8">
-                <img class="mySlides" src="https://www.w3schools.com/w3css/img_ny.jpg" style="width:100%">
-                <img class="mySlides" src="https://www.w3schools.com/w3css/img_la.jpg" style="width:100%">
-                <img class="mySlides" src="https://www.w3schools.com/w3css/img_chicago.jpg" style="width:100%">
+                <img class="mySlides" src="https://cdn.pixabay.com/photo/2018/11/17/20/18/fiber-3821957__340.jpg" style="width:100%">
+                <img class="mySlides" src="https://cdn.pixabay.com/photo/2014/01/16/10/36/fiber-optic-cable-246270__340.jpg" style="width:100%">
+                <img class="mySlides" src="https://cdn.pixabay.com/photo/2018/01/08/17/37/luminescence-3069828__340.jpg" style="width:100%">
                 <div class="overlay"></div>
             </div>
            <div class="col-4 Slidshow_imagens_cortadas">
                 <div class="Div_imagens_cortadas">
-                    <img src="https://www.w3schools.com/w3css/img_chicago.jpg" style="width:100%">
+                    <img src="https://image.shutterstock.com/image-photo/network-cables-closeup-fiber-optical-260nw-634322429.jpg" style="width:100%">
                 </div>
                 <div class="Div_imagens_cortadas">
-                    <img src="https://www.w3schools.com/w3css/img_chicago.jpg" style="width:100%">
+                    <img src="https://image.shutterstock.com/image-photo/optical-fiber-260nw-449934169.jpg" style="width:100%">
                 </div>
                 <div class="Div_imagens_cortadas_3">
-                    <img src="https://www.w3schools.com/w3css/img_chicago.jpg" style="width:100%">
+                    <img src="https://cdn.pixabay.com/photo/2016/07/14/02/01/fibre-1515964__340.jpg" style="width:100%">
                 </div>
            </div>
         </div>
+        <div class="row">
+            <div class="col-6 div_Video_e_texto">
+                <video controls>
+                    <source src="../Files/FilesSended/2.mp4" type="video/mp4">
+                    Your browser does not support HTML5 video.
+                </video>
+            </div>
+            <div class="col-6 div_Video_e_texto">
+                <span class="span_Video_e_texto">
+                    <h1>Lorem ipsum dolor</h1>
+                    <br>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Viverra tellus in hac
+                    habitasse platea dictumst. Aliquam id diam maecenas ultricies mi eget mauris
+                    pharetra. Enim praesent elementum facilisis leo. Cum sociis natoque penatibus
+                    et magnis dis parturient. Pellentesque pulvinar pellentesque habitant morbi
+                    tristique senectus et. Quam adipiscing vitae
+                </p>
+            </div>
+        </div>
     </div>
-</body>
 
+   
+        
+    
+</body>
+<?php
+    include_once "Content/_Footer.html";
+?>
 <?php
 include_once "Content/_scripts.html";
 ?>
